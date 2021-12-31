8aa096850b803bd200b65fa5da7012507ed96b43
kabilan1290 <tamiltuts1290@gmail.com>
2021-12-31 14:03:12 +0530

    CTF tool

--- crt.py
+++ crt.py
@@ -1,5 +1,5 @@
-import libnum
-import math
+from sympy import root
+from sympy.ntheory.modular import crt
 from Crypto.Util.number import *
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 prGreen("Give me n1 :")
@@ -15,26 +15,15 @@ c2 = int(input(""))
 prGreen("Give me c3 :")
 c3 = int(input(""))
 e=3
-rem = [c1,c2,c3]
 
-mod = [n1,n2,n3]
-
-res=libnum.solve_crt(rem,mod)
-
-#print(res)
-
-val = math.log10(res)/e
-
-#print ("\nLog(res)/e: %d" %val)
-
-val=10**(math.log10(res)/e)
+m=int(root(crt([n1,n2,n3],[c1,c2,c3])[0],3))
 
 
-prGreen("\nHere we go!: %s " % long_to_bytes(int(round(val))))
+prGreen("\nHere we go!: %s " % long_to_bytes(m))
 
-val = round(val)
-val = str(val)
-prGreen("\nHere we go again!: %s " % bytes.fromhex(val))
+#val = round(val)
+#val = str(val)
+#prGreen("\nHere we go again!: %s " % bytes.fromhex(val))
 
 #https://medium.com/asecuritysite-when-bob-met-alice/cracking-the-chinese-python-way-89b4e675c63c
 
